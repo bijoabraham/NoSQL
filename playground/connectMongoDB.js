91c161ea77c188502e265c2026ffc7cb9507f789
Bijo Abraham <bijoabraham@gmail.com>
2018-04-01 22:12:38 -0500

    Object ID, ES 6 destructuring

--- playground/connectMongoDB.js
+++ playground/connectMongoDB.js
@@ -1,6 +1,10 @@
-const mongodb = require("mongodb").MongoClient;
+//const mongodb = require("mongodb").MongoClient;
+//ES 6 destructuring
+const {MongoClient,ObjectID} = require("mongodb")
 
-mongodb.connect("mongodb://localhost:27017/NotesApp",(err,client)=>{
+var id = new ObjectID();
+console.log(`New object id :${id}`);
+MongoClient.connect("mongodb://localhost:27017/NotesApp",(err,client)=>{
     if(err){
         return "Mongo DB connection failed";
     }
@@ -11,7 +15,7 @@ mongodb.connect("mongodb://localhost:27017/NotesApp",(err,client)=>{
         if(err){
             return console.log("Failed to insert record",err);
         }
-        //console.log(JSON.stringify(result.ops,undefined,2));        
+        console.log("Record created time from object id :"+result.ops[0]._id.getTimestamp());        
     });
 
     db.collection("Users").insertOne({name:"test", location:"test location"},(err,result)=>{
